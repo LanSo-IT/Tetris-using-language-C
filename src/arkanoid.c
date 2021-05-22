b7c9df1f550d2655076b5cbcb2676b979ea4a695
Burckert <lburckert0@gmail.com>
2021-05-22 11:25:56 +0200

    ajout SDL Image - code - creation png background - pas terminé

--- src/arkanoid.c
+++ src/arkanoid.c
@@ -5,6 +5,7 @@
 #include <windows.h>
 #include <conio.h>
 #include <time.h>
+#include <SDL_image.h>
 
 #include "model_tetris.h"
 
@@ -51,8 +52,9 @@ void init()
 	pWindow = SDL_CreateWindow("Tetris", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 960, 672, SDL_WINDOW_SHOWN);
 	win_surf = SDL_GetWindowSurface(pWindow);
 	//plancheSprites = SDL_LoadBMP("../bin/default_cell.bmp");
-	BackgroundSprites = SDL_LoadBMP("../bin/wall_cell.bmp");
-	SDL_SetColorKey(plancheSprites, true, 0);  // 0: 00/00/00 noir -> transparent
+	//BackgroundSprites = SDL_LoadBMP("../bin/wall_cell.bmp");
+	BackgroundSprites = IMG_Load("../bin/metal-background.png");
+	//SDL_SetColorKey(plancheSprites, true, 0);  // 0: 00/00/00 noir -> transparent
 }
 
 
@@ -68,6 +70,8 @@ void drawBackground()
 			dest.y = j;
 			SDL_BlitSurface(BackgroundSprites, &srcBlock, win_surf, &dest);
 		}
+
+	SDL_FreeSurface(BackgroundSprites);
 }
 
 /* Function that draw a grid of blocks
